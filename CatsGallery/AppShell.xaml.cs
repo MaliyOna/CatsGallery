ebbe2e4e8b637238be6fca423ab45291a8e923c3
MaliyOna <veronika.pr777@gmail.com>
2024-12-04 13:11:58 +0100

    first commit

---
+++ CatsGallery/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CatsGallery
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CatsGallery/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CatsGallery
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
