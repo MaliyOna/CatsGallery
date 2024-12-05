1143deee0700b4ca507a5c1fc125003d8a221cf8
MaliyOna <veronika.pr777@gmail.com>
2024-12-05 12:11:51 +0300

    add binding and dublication of cats

---
+++ CatsGallery/Models/CatModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CatsGallery.Models;
+
+public class CatModel
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string ImageSource { get; set; } = string.Empty;
+}
