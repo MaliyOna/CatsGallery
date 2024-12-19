08ae2b68b8ed87a94769d14ebc8a7cb6affd813c
MaliyOna <veronika.pr777@gmail.com>
2024-12-19 12:10:54 +0300

    add  BindableProperty

--- CatsGallery/Controls/CatCardView.xaml.cs
+++ CatsGallery/Controls/CatCardView.xaml.cs
@@ -2,9 +2,24 @@ namespace CatsGallery.Controls
 {
     public partial class CatCardView : ContentView
     {
+        public static readonly BindableProperty CatModelProperty =
+            BindableProperty.Create(nameof(CatModel), typeof(object), typeof(CatCardView), propertyChanged: OnCatModelChanged);
+
+        public object CatModel
+        {
+            get => GetValue(CatModelProperty);
+            set => SetValue(CatModelProperty, value);
+        }
+
         public CatCardView()
         {
             InitializeComponent();
+        }
+
+        private static void OnCatModelChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (CatCardView)bindable;
+            control.BindingContext = newValue;
         }
     }
 }
